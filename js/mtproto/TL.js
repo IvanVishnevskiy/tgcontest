712188a84ac340bfc98e204552359431272ace03
Ivan Astapov <io@forgetable.ru>
2019-11-19 20:35:16 +0300

    Working on deserialization

--- js/mtproto/TL.js
+++ js/mtproto/TL.js
@@ -4,12 +4,27 @@ import getMessageID from '../helpers/getMessageID'
 
 import bigInt from 'big-integer'
 
-class Types {
+class TypesIn {
   static int = (data = 0, length = 128) => {
     data = data.map ? data : 
     typeof data === 'string' ? Bytes.fromHex(bigInt(data).toString(16)) : 
     Bytes.fromInt(data, length / 8)
     return data.length === length / 8 ? data : Bytes.addPadding(data.reverse(), length / 8)
+  }
+}
+
+class TypesOut {
+  static int = (data = [], length = 128) => {
+    length = length / 8
+    let item = new Array(length)
+    for(let i = 0; i < length; i++) item[i] = data[i]
+    const hexItem = Bytes.toHex(item)
+    item = (length >= 8 && parseInt(hexItem, 16) !== 0) ? hexItem : Bytes.toInt(item) 
+    return { item, res: data.slice(length) }
+  }
+  static bytes = (data = []) => ({ res: [], item: data })
+  static string = (data = []) => {
+    console.log(data)
   }
 }
 
@@ -28,19 +43,19 @@ class Serialization {
       const { name, type } = param
       const inputParam = inputParams[name]
       if(!inputParam) console.info(`No param ${name} provided for object ${inputName}`)
-      this.bytes = this.bytes.concat(Types[type.fieldType](inputParams[name] || [], type.length))
+      this.bytes = this.bytes.concat(TypesIn[type.fieldType](inputParams[name] || [], type.length))
     })
     return output
   }
 
   getBytes = () => {
     const { bytes } = this
-    const authKey = Types.int(0, 64)
+    const authKey = TypesIn.int(0, 64)
     const id = getMessageID()
     const res = [].concat(
       authKey,
-      Types.int(id, 64),
-      Types.int(bytes.length, 32).reverse(),
+      TypesIn.int(id, 64),
+      TypesIn.int(bytes.length, 32).reverse(),
       bytes
     )
     return res
@@ -53,14 +68,26 @@ class Deserialization {
   constructor(name, data) {
     if(name && data) this.deserialize(name, data)
   }
-  deserialize = (name = '', data = []) => {
+  
+  fields = {}
+  deserialize = (name = '', data = [], rec) => {
+    console.log(data)
     if(!name) throw new Error('No name')
     data = data.byteLength ? [...new Uint8Array(data)] : data
     if(!data || !data.length) throw new Error('Nothing to deserialize')
     if(data.length % 4) throw new Error('Data length should be divisible by 4')
     const object = schema[name.toLowerCase()]
     if(!object) throw new Error('No object for: ' + name)
-    console.log(object, data)
+    const skeleton = schema.notloggedskeleton
+    if(!rec && !skeleton) throw new Error('No skeleton found');
+    (rec ? object.params : skeleton.params).forEach(field => {
+      const { name, type } = field
+      const { res, item } = TypesOut[type.fieldType](data || [], type.length)
+      data = res
+      this.fields[name] = item
+    })
+    console.log(this.fields)
+    this.deserialize(name, this.fields.message_data, true)
   }
 }
 
