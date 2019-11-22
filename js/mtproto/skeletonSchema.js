3920ef93ce68f035f4f1a4be06740a865baa161d
Ivan Astapov <io@forgetable.ru>
2019-11-22 21:01:32 +0300

    Added name to skeleton

--- js/mtproto/skeletonSchema.js
+++ js/mtproto/skeletonSchema.js
@@ -1,3 +1,3 @@
 export default `
-  notLoggedSkeleton# auth_key_id:int64 message_id:int64 message_data_length:int32 message_data:bytes = _;
+  notLoggedSkeleton# auth_key_id:int64 message_id:int64 message_data_length:int32 name:name message_data:bytes = _;
 `
